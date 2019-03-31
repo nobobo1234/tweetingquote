977799768d2fba7a373cccf5b515f84cb612ff1b
nola1222 <noahvanboven@gmail.com>
2019-03-31 16:14:19 +0200

    Being more consistent with quotes

--- bot.py
+++ bot.py
@@ -8,36 +8,36 @@ from PIL import ImageFont, Image, ImageDraw
 import yaml
 
 # Open config
-config = yaml.load(open('./config.yml', 'r'), Loader=yaml.FullLoader)
+config = yaml.load(open("./config.yml", "r"), Loader=yaml.FullLoader)
 
 # Connect to api
 api = Twython(
-	config['consumer_key'],
-	config['consumer_secret'],
-	config['access_token'],
-	config['access_token_secret'])
+	config["consumer_key"],
+	config["consumer_secret"],
+	config["access_token"],
+	config["access_token_secret"])
 
 # Getting data from Quote of the Day api
-data = feedparser.parse('https://www.brainyquote.com/link/quotebr.rss')
-author = data['entries'][0]['title']
+data = feedparser.parse("https://www.brainyquote.com/link/quotebr.rss")
+author = data["entries"][0]["title"]
 quote = f"{data['entries'][0]['summary']} ~ {author}"
-link = data['entries'][0]['link']
+link = data["entries"][0]["link"]
 
-background_request = requests.get('https://api.unsplash.com/photos/random?query=nature',
+background_request = requests.get("https://api.unsplash.com/photos/random?query=nature",
 								headers={"Authorization": f"Client-ID {config['unsplash_client_id']}"})
 background_data = background_request.json()
-background_image = background_data['urls']['raw'] + "&w=1080"
+background_image = background_data["urls"]["raw"] + "&w=1080"
 r = requests.get(background_image, stream=True)
 if r.status_code == 200:
-	with open('scenery.png', 'wb') as f:
+	with open("scenery.png", "wb") as f:
 		for chunk in r:
 			f.write(chunk)
 
 # Making image
-font = ImageFont.truetype('arial.ttf', 30)
+font = ImageFont.truetype("arial.ttf", 30)
 
 image = Image.open("scenery.png").convert("RGBA")
-text = Image.new('RGBA', image.size, color=(0, 0, 0, 0))
+text = Image.new("RGBA", image.size, color=(0, 0, 0, 0))
 draw = ImageDraw.Draw(image)
 text_draw = ImageDraw.Draw(text)
 
@@ -57,7 +57,7 @@ text_draw.text((x, y), quote_lines, font=font, align="center")
 final = Image.alpha_composite(image, text)
 final.save("image.png")
 
-photo = open('image.png', 'rb')
+photo = open("image.png", "rb")
 response = api.upload_media(media=photo)
-api.update_status(media_ids=[response['media_id']],
+api.update_status(media_ids=[response["media_id"]],
 				status=f"Hello, here's my daily tweet #quote by {author}. {link}")
